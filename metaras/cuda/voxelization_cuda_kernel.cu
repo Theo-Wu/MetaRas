df313c03d184a0030613348f317c16f665b802c7
Theo-Wu <anykis33@gmail.com>
2024-06-20 16:34:16 +0100

    commented out the new atomicAdd

--- metaras/cuda/voxelization_cuda_kernel.cu
+++ metaras/cuda/voxelization_cuda_kernel.cu
@@ -13,19 +13,21 @@
 #include <stdio.h>
 
 // for the older gpus atomicAdd with double arguments does not exist
-#if  __CUDA_ARCH__ < 600 and defined(__CUDA_ARCH__)
-static __inline__ __device__ double atomicAdd(double* address, double val) {
-    unsigned long long int* address_as_ull = (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                __double_as_longlong(val + __longlong_as_double(assumed)));
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
-    } while (assumed != old);
-    return __longlong_as_double(old);
-}
-#endif
+// if using CUDA 8.0 or older please uncomment this 
+
+// #if  __CUDA_ARCH__ < 600 and defined(__CUDA_ARCH__)
+// static __inline__ __device__ double atomicAdd(double* address, double val) {
+//     unsigned long long int* address_as_ull = (unsigned long long int*)address;
+//     unsigned long long int old = *address_as_ull, assumed;
+//     do {
+//         assumed = old;
+//         old = atomicCAS(address_as_ull, assumed,
+//                 __double_as_longlong(val + __longlong_as_double(assumed)));
+//     // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
+//     } while (assumed != old);
+//     return __longlong_as_double(old);
+// }
+// #endif
 
 
 
